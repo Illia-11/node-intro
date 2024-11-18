03520211fa5bc9766a6336207888e8c9bafa7754
Illia Piddubnyi <illya2008zp@gmail.com>
2024-11-18 20:29:18 +0200

    Add ESModules syntax example

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
+// Імпорт даних з інших модулей
+
+// 1. Імпорт за замовчанням
+// import MyMath from './myMath.js'
+
+// при імпорті за замовчанням назва необовʼязково має співпадати
+import TheBestMath from './myMath.js'
+
+// 2. Іменний імпорт
+import {PIValue, testFunc as newTest, MyBoolAlgebra, MyTrigonometryMath} from './myMath.js'
+
+// 3. Комбінований імпорт
+// import MyMath, {MyBoolAlgebra, MyTrigonometryMath} from './myMath.js'
+
+import TheBestMath, {PIValue, testFunc as newTest, MyBoolAlgebra, MyTrigonometryMath} from './myMath.js'
+
+import * as AllExportsFromMyMath from './myMath.js'
+
 console.log('Hello from Node')
 
-console.log(0.1 + 0.2)
+console.log(AllExportsFromMyMath)
 
-const sum = (...nums) => nums.reduce((acc, num) => acc + num)
+console.log(PIValue)
+
+newTest()
+
+function testFunc() {
+
+}
 
 const numbers = [1,2,3,4,5,6,7,8,9,-1]
 
-console.log(sum(...numbers))
+// console.log(sum(...numbers))
+
+console.log(TheBestMath.sum(...numbers))
+// console.log(MyMath.sum(...numbers))
---
+++ myMath.js
@@ -0,0 +1,43 @@
+class MyMath {
+  static sum = (...nums) => nums.reduce((acc, num) => acc + num)
+
+  static substract = (...nums) => nums.reduce((acc, num) => acc - num)
+
+  static multiply = (...nums) => nums.reduce((acc, num) => acc * num)
+
+  static divide = (...nums) => nums.reduce((acc, num) => acc / num)
+}
+
+class MyTrigonometryMath {
+  static func1 = () => {}
+}
+
+class MyBoolAlgebra {
+  static func2 = () => {}
+}
+
+// Експорт даних з модуля
+
+// 1. Експорт за замовчанням - може використовуватися один раз за модуль
+export default MyMath
+
+// export default 10
+
+// const obj = {
+//   // MyTrigonometryMath: MyTrigonometryMath
+//   MyTrigonometryMath
+// }
+
+// obj.MyTrigonometryMath
+
+// 2. Іменний експорт
+export {
+  MyTrigonometryMath,
+  MyBoolAlgebra
+}
+
+export const PIValue = 3.14
+
+export function testFunc() {
+  console.log('testFunction')
+}
